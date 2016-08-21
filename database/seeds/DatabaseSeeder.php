4a2f0e380f0e3490a1a4249e3f9c11c810077fb7
Chris Gmyr <cmgmyr@gmail.com>
2016-08-20 21:18:11 -0400

    update users seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,22 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        App\User::create([
-            'name' => 'Chris Gmyr',
-            'email' => 'chris@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
-
-        App\User::create([
-            'name' => 'Taylor Otwell',
-            'email' => 'taylor@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
-
-        App\User::create([
-            'name' => 'Adam Wathan',
-            'email' => 'adam@example.com',
-            'password' => Hash::make('pass123'),
-        ]);
+        $this->call(UsersTableSeeder::class);
     }
 }
